8ba7e8bac8792e4f9b53614282dfd0bcd40ad4d1
Dorine Lemière <dorine.lemiere@epita.fr>
2019-09-25 21:40:24 +0200

    AwakoPlayer: randomize first more

--- src/Game/PlayerIA/AwakoPlayer.php
+++ src/Game/PlayerIA/AwakoPlayer.php
@@ -20,9 +20,13 @@ class AwakoPlayer extends Player
 
     public function getChoice()
     {
+      // Return default choice based on psychological stats to win for the first round, because there is no history
       if (!$this->result->getLastChoiceFor($this->mySide)) {
-        return parent::paperChoice();
+        $arr = array('a'=>parent::paperChoice(), 'b'=>parent::rockChoice(), 'c'=>parent::scissorsChoice());
+        shuffle(arr);
+        return $arr[0];
       }
+      // add last choices to make an history in order to make statistics
       if ($this->result->getLastChoiceFor($this->opponentSide) == parent::rockChoice())
         $this->rock++;
       else if ($this->result->getLastChoiceFor($this->opponentSide) == parent::paperChoice())
@@ -30,11 +34,12 @@ class AwakoPlayer extends Player
       else
         $this->scissors++;
 
+      // choose the equal move to max move of opponent history. if rocks appears more, for example, I play rocks. Rock'n'Roll baby ! ;)
       if ($this->rock >= $this->scissors && $this->rock >= $this->paper)
         return parent::rockChoice();
       else if ($this->paper >= $this->scissors && $this->paper >= $this->rock)
         return parent::paperChoice();
       else
-        return parent::scissorsChoice();       
-  }
+        return parent::scissorsChoice();     
+    }
 };
